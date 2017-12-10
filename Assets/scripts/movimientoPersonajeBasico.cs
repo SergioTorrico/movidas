68ebad3a49c06484769f60d9542b0e4140440c2e
SergioTorrico <segatorrico@gmail.com>
2017-12-10 11:51:40 +0100

    initial commit

---
+++ Assets/scripts/movimientoPersonajeBasico.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoPersonajeBasico : MonoBehaviour {
+    public float speed = 6f;
+    Vector2 movement;
+    Rigidbody2D playerRigidBody;
+    
+    void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+   
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        
+        Move(h, v);
+    }
+    void Move(float h,float v)
+    {
+        
+        movement.Set(h, v);
+        movement = movement * speed * Time.deltaTime;
+        playerRigidBody.MovePosition( playerRigidBody.position + movement);
+    }
+}
